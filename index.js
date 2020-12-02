1d3a60a1e2e4c043f5dd16077e46b0634c8508f2
Amanda Ciobanu <amanda@ciobanu.org>
2020-12-02 11:19:42 +0000

    feat: cookie modal

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
+$(document).ready(function () {
+  const cookieContainer = document.querySelector('.cookie-container');
+  const cookieButton = document.querySelector('.cookie-button');
+
+
+    cookieButton.addEventListener('click', ()=> {
+      cookieContainer.classList.remove('active');
+      localStorage.setItem('cookieBannerDisplay', 'true')
+    })
+
+
+  setTimeout(() => {
+    if(!localStorage.getItem('cookieBannerDisplay')) {
+      cookieContainer.classList.add('active')
+    }
+  }, 2000);
+
+});
+
+
 
